0fcef54693093bc84ee8cd189bc0efb00db18622
TerenceGa <tg6709032@gmail.com>
2024-04-28 15:12:44 -0400

    js fixed

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function showImage()
     const imageContainer = document.getElementById('imageContainer');
     const states = {
         massachusetts: 'src/mass.jpg', // Replace with actual path
-        connecticut: 'src/Conne.jpeg',
-        dc: 'src/whitehouse.jpg',
+        connecticut: 'src/conn.jpg',
+        dc: 'src/dc.jpg',
         'new york': 'src/nyc.jpg',
         california: 'src/ca.jpg',
 
@@ -18,9 +18,7 @@ function showImage()
         img.src = states[stateInput];
         imageContainer.appendChild(img);
     }
-    else if (states == "answertopic"){
-        imageContainer.innerHTML = '<p>nice catch but still got you</p>'
-    }
+
     else {
         imageContainer.innerHTML = '<p>No image found for this state. Please check your spelling and try again.</p>';
     }
